805ec6230804ca65b37ce8749a3fd447c1a87ff5
Mehran Hamedani Nezhad <mehran.hamedaninezhad@gmail.com>
2019-09-02 16:19:56 +0430

    cors

--- main.go
+++ main.go
@@ -6,12 +6,17 @@ import (
 	"pa-back/config"
 	_ "pa-back/repository"
 	"pa-back/routes"
+
+	"github.com/rs/cors"
 )
 
 func main() {
 	router := routes.GetRouter()
 	fmt.Println("listen on " + config.JSONConfig.Host + ":" + config.JSONConfig.Port)
-	err := http.ListenAndServe(fmt.Sprintf("%s:%s", config.JSONConfig.Host, config.JSONConfig.Port), router)
+	handler := cors.Default().Handler(router)
+	err := http.ListenAndServe(
+		fmt.Sprintf("%s:%s", config.JSONConfig.Host, config.JSONConfig.Port),
+		handler)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
